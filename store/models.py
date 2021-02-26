0a3bc482d6381a7cad6ca82802b24f1e0a40a34c
Rashad Humphrey <rashadthecoder@gmail.com>
2021-02-26 08:39:05 -0600

    store app

--- store/models.py
+++ store/models.py
@@ -3,9 +3,9 @@ import datetime
 
 # Create your models here.
 class Library(models.Model):
-    title = models.CharField(max_length=100)
-    director = models.CharField(max_length=100)
-    writer = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=200)
+    writer = models.CharField(max_length=200)
     description = models.TextField()
     publish_date = models.DateField(default=datetime.date.today)
     price = models.DecimalField(decimal_places=2, max_digits=8)
